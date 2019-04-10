a595381dd91201fcc7aa297bc1b21c889e9b8fe9
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-10 22:06:47 +0530

    Handled the functions for getUsername, getId

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/complaintapp');
+mongoose.connect('mongodb://localhost/complaintapp', { useNewUrlParser: true });
 const bcrypt = require('bcryptjs');
 
 // User Schema
@@ -30,4 +30,20 @@ module.exports.registerUser = function (newUser, callback) {
             newUser.save(callback);
         });
     });
+}
+
+module.exports.getUserByUsername = function(username, callback){
+    const query = {username: username}
+    User.findOne(query, callback);
+  }
+  
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if(err) throw err;
+        callback(null, isMatch);
+});
 }
