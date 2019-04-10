a3c987ef31a5f9fe123d08a1117ab6a2b8131771
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-10 22:08:02 +0530

    Added the flash msg for error

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ app.use(flash());
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
