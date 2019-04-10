948efea9e2484e310acf11e12d4adae53799fbb4
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-10 21:37:04 +0530

    Added New User Creation ,added path for Login

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,58 @@
 var express = require('express');
-var router  =  express.Router();
+var router = express.Router();
 
+let User = require('../models/user');
 
-//HOME PAGE
-router.get('/', (req,res,next)=>{
-    res.render('index');
+// Home Page
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
 
-//REGISTER
-router.get('/register', (req,res,next)=>{
-    res.render('register');
+// Login Form
+router.get('/login', (req, res, next) => {
+  res.render('login');
 });
 
+// Register Form
+router.get('/register', (req, res, next) => {
+  res.render('register');
+});
 
 // Process Register
 router.post('/register', (req, res, next) => {
-    const name = req.body.name;
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const password2 = req.body.password2;
-  
-    req.checkBody('name', 'Name field is required').notEmpty();
-      req.checkBody('email', 'Email field is required').notEmpty();
-      req.checkBody('email', 'Email must be a valid email address').isEmail();
-      req.checkBody('username', 'Username field is required').notEmpty();
-      req.checkBody('password', 'Password field is required').notEmpty();
-      req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-  
-    let errors = req.validationErrors();
-  
-    if(errors){
-      res.render('register', {
-        errors: errors
-      });
-    } else {
-      console.log('SUCCESS');
-      return;
-  
-    }
-  });
-  
+  const name = req.body.name;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('name', 'Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required').notEmpty();
+	req.checkBody('email', 'Email must be a valid email address').isEmail();
+	req.checkBody('username', 'Username field is required').notEmpty();
+	req.checkBody('password', 'Password field is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('register', {
+      errors: errors
+    });
+  } else {
+    const newUser = new User({
+      name: name,
+      username: username,
+      email: email,
+      password: password
+    });
+
+    User.registerUser(newUser, (err, user) => {
+      if(err) throw err;
+      req.flash('success_msg', 'You are registered and can log in');
+      res.redirect('/login');
+    });
+  }
+});
 
 module.exports = router;
