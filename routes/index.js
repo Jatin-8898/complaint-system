1f42ca25c00ef44e33fc222cdb5cb39f33e8db9f
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-10 21:54:40 +0530

    Included the Passport Stategies

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 let User = require('../models/user');
 
@@ -53,6 +55,46 @@ router.post('/register', (req, res, next) => {
       res.redirect('/login');
     });
   }
+});
+
+// Local Strategy
+passport.use(new LocalStrategy((username, password, done) => {
+  User.getUserByUsername(username, (err, user) => {
+    if(err) throw err;
+    if(!user){
+      return done(null, false, {message: 'No user found'});
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if(err) throw err;
+      if(isMatch){
+        return done(null, user);
+      } else {
+        return done(null, false, {message: 'Wrong Password'});
+      }
+    });
+  });
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.getUserById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+// Login Processing
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect:'/',
+    failureRedirect:'/login',
+    failureFlash: true
+  }, (req, res) => {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
