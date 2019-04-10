3913fd44cfe79097c2439743d9a979aea4657bab
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-10 20:44:24 +0530

    Added the Express Session,Messages & Validator

--- app.js
+++ app.js
@@ -23,6 +23,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Express messages
+app.use(require('connect-flash')());
+app.use((req, res, next) => {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+      let namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use('/', index);
+
+
 // Start Server
 app.listen(port, () => {
   console.log('Server started on port '+port);
