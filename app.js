0679d476b4a733ff2469b08dac8fa6aa60ed37df
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-10 21:44:55 +0530

    Changed the flash msg for success & error

--- app.js
+++ app.js
@@ -33,9 +33,10 @@ app.use(session({
 }));
 
 // Express messages
-app.use(require('connect-flash')());
+app.use(flash());
 app.use((req, res, next) => {
-  res.locals.messages = require('express-messages')(req, res);
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
   next();
 });
 
