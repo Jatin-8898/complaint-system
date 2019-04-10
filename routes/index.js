b09d15adb38d9150b57c15b14fafc1d0fb2e5478
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-10 21:19:57 +0530

    Handled the post request for Register

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,33 @@ router.get('/register', (req,res,next)=>{
 });
 
 
+// Process Register
+router.post('/register', (req, res, next) => {
+    const name = req.body.name;
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+  
+    req.checkBody('name', 'Name field is required').notEmpty();
+      req.checkBody('email', 'Email field is required').notEmpty();
+      req.checkBody('email', 'Email must be a valid email address').isEmail();
+      req.checkBody('username', 'Username field is required').notEmpty();
+      req.checkBody('password', 'Password field is required').notEmpty();
+      req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+  
+    let errors = req.validationErrors();
+  
+    if(errors){
+      res.render('register', {
+        errors: errors
+      });
+    } else {
+      console.log('SUCCESS');
+      return;
+  
+    }
+  });
+  
+
 module.exports = router;
