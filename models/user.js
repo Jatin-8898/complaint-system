ff59dd827128538e323091a5745dbac8c83d11c4
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-10 21:36:30 +0530

    Create the model for the User

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/passportapp');
+const bcrypt = require('bcryptjs');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.registerUser = function (newUser, callback) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) {
+                console.log(err);
+            }
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
