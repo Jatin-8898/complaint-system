b0342a37f51b84220ebb45fbf07a081dee191acf
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-11 18:09:46 +0530

    Solved major Session Issue

--- routes/index.js
+++ routes/index.js
@@ -7,109 +7,128 @@ let User = require('../models/user');
 
 // Home Page - Dashboard
 router.get('/', ensureAuthenticated, (req, res, next) => {
-  res.render('index');
+    res.render('index');
 });
 
 // Login Form
 router.get('/login', (req, res, next) => {
-  res.render('login');
+    res.render('login');
 });
 
 // Register Form
 router.get('/register', (req, res, next) => {
-  res.render('register');
+    res.render('register');
 });
 
 // Logout
 router.get('/logout', (req, res, next) => {
-  req.logout();
-  req.flash('success_msg', 'You are logged out');
-  res.redirect('/login');
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/login');
 });
+
+//Complaint
+router.get('/complaint', (req, res, next) => {
+    //console.log(req.session.passport.username);
+    //console.log(user.name);
+    res.render('complaint', {
+        username: req.session.user,
+    });
+});
+
 
 // Process Register
 router.post('/register', (req, res, next) => {
-  const name = req.body.name;
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
-  const password2 = req.body.password2;
+    const name = req.body.name;
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const password2 = req.body.password2;
 
-  req.checkBody('name', 'Name field is required').notEmpty();
-	req.checkBody('email', 'Email field is required').notEmpty();
-	req.checkBody('email', 'Email must be a valid email address').isEmail();
-	req.checkBody('username', 'Username field is required').notEmpty();
-	req.checkBody('password', 'Password field is required').notEmpty();
-	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('email', 'Email must be a valid email address').isEmail();
+    req.checkBody('username', 'Username field is required').notEmpty();
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
-  let errors = req.validationErrors();
+    let errors = req.validationErrors();
 
-  if(errors){
-    res.render('register', {
-      errors: errors
-    });
-  } else {
-    const newUser = new User({
-      name: name,
-      username: username,
-      email: email,
-      password: password
-    });
+    if (errors) {
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        const newUser = new User({
+            name: name,
+            username: username,
+            email: email,
+            password: password
+        });
 
-    User.registerUser(newUser, (err, user) => {
-      if(err) throw err;
-      req.flash('success_msg', 'You are registered and can log in');
-      res.redirect('/login');
-    });
-  }
+        User.registerUser(newUser, (err, user) => {
+            if (err) throw err;
+            req.flash('success_msg', 'You are registered and can log in');
+            res.redirect('/login');
+        });
+    }
 });
 
 // Local Strategy
 passport.use(new LocalStrategy((username, password, done) => {
-  User.getUserByUsername(username, (err, user) => {
-    if(err) throw err;
-    if(!user){
-      return done(null, false, {message: 'No user found'});
-    }
+    User.getUserByUsername(username, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return done(null, false, {
+                message: 'No user found'
+            });
+        }
 
-    User.comparePassword(password, user.password, (err, isMatch) => {
-      if(err) throw err;
-      if(isMatch){
-        return done(null, user);
-      } else {
-        return done(null, false, {message: 'Wrong Password'});
-      }
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, {
+                    message: 'Wrong Password'
+                });
+            }
+        });
     });
-  });
 }));
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+    var sessionUser = {
+        _id: user._id,
+        name: user.name,
+        email: user.email
+    }
+    done(null, sessionUser);
 });
 
 passport.deserializeUser((id, done) => {
-  User.getUserById(id, (err, user) => {
-    done(err, user);
-  });
+    User.getUserById(id, (err, sessionUser) => {
+        done(err, sessionUser);
+    });
 });
 
 // Login Processing
 router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect:'/',
-    failureRedirect:'/login',
-    failureFlash: true
-  })(req, res, next);
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })(req, res, next);
 });
 
 // Access Control
-function ensureAuthenticated(req, res, next){
-  if(req.isAuthenticated()){
-    return next();
-  } else {
-    req.flash('error_msg', 'You are not authorized to view that page');
-    res.redirect('/login');
-  }
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        req.flash('error_msg', 'You are not authorized to view that page');
+        res.redirect('/login');
+    }
 }
 
 module.exports = router;
